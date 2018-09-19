411c8d19cc4e7e20b3969e00890b83faef0814f2
Richard Williams <richard.williams2@manchester.ac.uk>
2018-09-19 17:04:37 +0100

    Add note func

--- src/controllers/practiceIndicatorController.js
+++ src/controllers/practiceIndicatorController.js
@@ -63,9 +63,20 @@ export default (
           }
         });
 
+        const patientLookup = {};
         summary.patients.forEach((p) => {
           p.indicatorNotesToDisplay = p.indicatorNotes.filter(note => p.indicators.filter(indicator => indicator.id === note.id).length > 0);
           p.isNote = !!p.patientNote || p.indicatorNotesToDisplay.length > 0;
+          p.indicators.forEach((indicator) => {
+            indicator.note = '';
+            p.indicatorNotes.forEach((note) => {
+              if (note.id === indicator.id) {
+                indicator.note = note.note;
+              }
+            });
+          });
+          p.patientNote = p.patientNote || '';
+          patientLookup[p.id] = p;
         });
 
         practices.forEach((p) => {
@@ -184,6 +195,92 @@ export default (
             }
           });
         };
+
+        document.getElementById('patientTable').addEventListener('click', (e) => {
+          if (e.target.tagName.toLowerCase() !== 'button') return;
+
+          const { type, patientId } = e.target.dataset;
+
+          if (type === 'delete') {
+            $('#modal')
+              .html(Template.it('modal-note-sure', { nhs: patientLookup[patientId].nhs, id: patientLookup[patientId].id }))
+              .modal();
+          } else {
+            $('#modal')
+              .html(Template.it('modal-note', { patient: patientLookup[patientId] }))
+              .modal();
+          }
+        });
+
+        const modalClick = (e) => {
+          if (e.target.classList.contains('close-me')) {
+            $('#modal').modal('hide');
+          } else if (e.target.tagName.toLowerCase() === 'button') {
+            if (e.target.classList.contains('btn-danger')) { // delete note
+              api.notesDelete(e.target.dataset.patientId).then(() => {
+                // update ui;
+                patientLookup[e.target.dataset.patientId].indicatorNotesToDisplay = [];
+                patientLookup[e.target.dataset.patientId].isNote = false;
+                patientLookup[e.target.dataset.patientId].patientNote = '';
+                patientLookup[e.target.dataset.patientId].indicatorNotes = [];
+
+                $(`td.note-field[data-patient-id=${e.target.dataset.patientId}]`).html(Template.it('note', patientLookup[e.target.dataset.patientId]));
+
+                $('#modal').modal('hide');
+              });
+            } else { // upsert note
+              const timeNow = new Date();
+
+              // create note
+              const note = {
+                patientNote: $('#patientNote').val(),
+                patientNoteUpdated: timeNow.toISOString(),
+                patientNoteUpdatedBy: $('#usersName').text(),
+                patientNoteUpdatedLocaleString: timeNow.toLocaleString('en-GB'),
+                indicatorNotes: [],
+              };
+
+              const indicatorTextareas = document.querySelectorAll('textarea.indicator-note');
+              for (let i = 0; i < indicatorTextareas.length; i += 1) {
+                if (indicatorTextareas[i].value && indicatorTextareas[i].value.replace(/ /g, '').length > 0) {
+                  note.indicatorNotes.push({
+                    id: +indicatorTextareas[i].dataset.indicatorId,
+                    name: indicatorTextareas[i].dataset.indicatorName,
+                    note: indicatorTextareas[i].value,
+                  });
+                }
+              }
+
+              // upsert note
+              api.notesUpsert(e.target.dataset.patientId, note).then(() => {
+                patientLookup[e.target.dataset.patientId].indicatorNotes = note.indicatorNotes;
+                patientLookup[e.target.dataset.patientId].indicatorNotesToDisplay = note.indicatorNotes.filter(nt => patientLookup[e.target.dataset.patientId].indicators.filter(indicator => indicator.id === nt.id).length > 0);
+                patientLookup[e.target.dataset.patientId].isNote = true;
+                patientLookup[e.target.dataset.patientId].indicators.forEach((indicator) => {
+                  indicator.note = '';
+                  patientLookup[e.target.dataset.patientId].indicatorNotes.forEach((nt) => {
+                    if (nt.id === indicator.id) {
+                      indicator.note = nt.note;
+                    }
+                  });
+                });
+                patientLookup[e.target.dataset.patientId].patientNote = note.patientNote;
+                patientLookup[e.target.dataset.patientId].patientNoteUpdatedBy = note.patientNoteUpdatedBy;
+                patientLookup[e.target.dataset.patientId].patientNoteUpdatedLocaleString = note.patientNoteUpdatedLocaleString;
+
+                $(`td.note-field[data-patient-id=${e.target.dataset.patientId}]`).html(Template.it('note', patientLookup[e.target.dataset.patientId]));
+
+                $('#modal').modal('hide');
+              });
+            }
+          }
+        };
+
+        const modalElement = document.getElementById('modal');
+        if (!modalElement.classList.contains('click-handler-added')) {
+          modalElement.addEventListener('click', modalClick);
+          modalElement.classList.add('click-handler-added');
+        }
 
         // add chart
         if (state.practiceIndicatorChartOrTable === 'chart') {
